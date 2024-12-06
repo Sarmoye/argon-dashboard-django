8eaa817ff3e0c126fd23b83402e8165ad6594000
Sarmoye <haidaraamitoure@gmail.com>
2024-12-06 17:42:26 +0100

    sources_data

--- apps/sources_data_app/views.py
+++ apps/sources_data_app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .models import *
 from .decorators import roles_required
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
 
-@roles_required(['superadmin'])
+""" @roles_required(['superadmin']) """
 def index(request):
     source_data = SourceData.objects.all()
     print(f'source_data ... {source_data}')
 
     context = {
+        'segment': 'index',
         'source_data': source_data,
     }
-    return render(request, 'sources_data/sources_home.html', context)
+    html_template = loader.get_template('sources_data/sources_home.html')
+    return HttpResponse(html_template.render(context, request))
