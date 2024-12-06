399f274492ead32f62c762218de31dc39a38ca4f
Sarmoye <haidaraamitoure@gmail.com>
2024-12-06 17:52:39 +0100

    review sunday

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,18 +8,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from apps.sources_data_app.models import *
 
 
 @login_required(login_url="/login/")
 def index(request):
-    source_data = SourceData.objects.all()
-    print(f'source_data ... {source_data}')
-
-    context = {
-        'segment': 'index',
-        'source_data': source_data,
-    }
+    context = {'segment': 'index'}
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
