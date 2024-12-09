7df255a57d6c64501f178189d825250a6850283b
Sarmoye <haidaraamitoure@gmail.com>
2024-12-09 15:03:17 +0100

    Update views.py

--- apps/sources_data_app/views.py
+++ apps/sources_data_app/views.py
@@ -1,26 +1,11 @@
-# sources/views.py
-
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 from django.contrib.auth.decorators import login_required
-from .models import SourceData
+from .models import SourceData  # Assurez-vous que le modèle est correctement importé
+from django.shortcuts import render
 
 @login_required(login_url='/authentication/login/')
 def index(request):
-    # Fetch and order the data
+    # Trier les données par timestamp en ordre décroissant
     source_data = SourceData.objects.all().order_by('-timestamp')
-    
-    # Pagination
-    page = request.GET.get('page', 1)  # Get the 'page' parameter from the request
-    paginator = Paginator(source_data, 10)  # 10 entries per page
-    
-    try:
-        paginated_data = paginator.page(page)
-    except PageNotAnInteger:
-        paginated_data = paginator.page(1)
-    except EmptyPage:
-        paginated_data = paginator.page(paginator.num_pages)
-
-    return render(request, 'sources_data/sources_home.html', {
-        'source_data': paginated_data,  # Pass only the current page's data
-    })
+    return render(request, 'sources_data/sources_home.html', {'source_data': source_data})
