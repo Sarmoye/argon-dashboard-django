a50646d4519c82e2c1302fd53de0918cbdec82c6
Sarmoye <haidaraamitoure@gmail.com>
2024-12-09 10:48:49 +0100

    url 'sources_home_app:sources_home' no more need pages from home

--- apps/sources_data_app/views.py
+++ apps/sources_data_app/views.py
@@ -2,19 +2,9 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 from .models import SourceData  # Assurez-vous que le modèle est correctement importé
+from django.shortcuts import render
 
-@login_required(login_url='/login/')  # Vérifie si l'utilisateur est authentifié
+@login_required(login_url='/login/')
 def index(request):
-    # Récupération des données depuis le modèle SourceData
     source_data = SourceData.objects.all()
-    print(f'source_data ... {source_data}')  # Debug pour voir les données dans la console
-
-    # Passer les données au template
-    context = {
-        'segment': 'index',
-        'source_data': source_data,
-    }
-
-    # Chargement et rendu du template
-    html_template = loader.get_template('sources_data/sources_home.html')
-    return HttpResponse(html_template.render(context, request))
+    return render(request, 'sources_data/sources_home.html', {'source_data': source_data})
