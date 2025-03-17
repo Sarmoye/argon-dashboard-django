886ae4f9abe3658e564043e4e7f63e1159aadd59
Sarmoye <haidaraamitoure@gmail.com>
2025-03-17 11:33:12 +0100

    Update models.py

--- apps/error_management_systems/models.py
+++ apps/error_management_systems/models.py
@@ -39,7 +39,7 @@ class ErrorType(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=['system_name', 'service_name', 'error_reason'], 
-                name='unique_system_error_reason1'
+                name='1unique_system_error_reason'
             )
         ]
     
