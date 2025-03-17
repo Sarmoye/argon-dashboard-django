36efd73beb2d35a942a28bebfc9e16ec5a58ca48
Sarmoye <haidaraamitoure@gmail.com>
2025-03-17 10:09:15 +0100

    error app

---
+++ error_management_systems/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ErrorManagementSystemsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'error_management_systems'
