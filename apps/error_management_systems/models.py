ef3ffe0b95d9645e881981cbde06b1de5fb071e0
Sarmoye <haidaraamitoure@gmail.com>
2025-03-26 15:04:47 +0100

    Update models.py

--- apps/error_management_systems/models.py
+++ apps/error_management_systems/models.py
@@ -226,7 +226,7 @@ class ErrorTicket(models.Model):
             self.ticket_reference = f"{type_id}"
 
         # La priorité est toujours l'impact_level de error_type
-        self.priorite = self.error_type.impact_level
+        self.priorite = self.error_type.impact_level.lower()
 
         # Enregistrement de la date de résolution si le statut est RESOLVED
         if self.statut == 'RESOLVED':
