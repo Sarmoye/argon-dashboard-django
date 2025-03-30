18587ee0b5daeac0f11e191fd387cd629e9a5f4c
Sarmoye <haidaraamitoure@gmail.com>
2025-03-30 13:03:23 +0100

    review url redirection

--- core/urls.py
+++ core/urls.py
@@ -9,12 +9,13 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
-    # Redirect root path to login
-    path('', lambda request: redirect('/authentication/login/')), # Auth routes - login / register
+    path("authentication/", include("apps.authentication.urls")), # Auth routes - login / register
     path("home/", include("apps.home.urls")),             # UI Kits Html files
     path("sources/", include("apps.sources_data_app.urls")),
     path("analysis/", include("apps.analysis.urls")),
     path("visualization/", include("apps.visualization.urls")),
     path("irm_dashboard/", include("apps.irm_dashboard.urls")),
     path("errors/", include("apps.error_management_systems.urls")),
+    # Redirect root path to login
+    path('', lambda request: redirect('/authentication/login/')),
 ]
