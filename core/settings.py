5c5abfb127ef13148cd4e021f7c33c6a4a12db79
Sarmoye <haidaraamitoure@gmail.com>
2025-05-13 12:37:53 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -239,7 +239,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     'process-cis-error-report': {
         'task': 'apps.error_management_systems.tasks.process_cis_error_report',
-        'schedule': timedelta(minutes=1),
+        'schedule': timedelta(minutes=5),
         'options': {'queue': 'queue_process_cis_error_report'},
      },
 }
