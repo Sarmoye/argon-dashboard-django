d862f48f548b32b2c2e955d925902ddd081e0ee5
Sarmoye <haidaraamitoure@gmail.com>
2025-05-13 11:07:42 +0100

    Update stop_workers.sh

--- stop_workers.sh
+++ stop_workers.sh
@@ -9,7 +9,7 @@ sudo systemctl stop celery-flower.service
 sudo systemctl disable celery-flower.service
 
 # Stop Celery workers for each queue
-for queue in execute_cis_error_report; do
+for queue in execute_cis_error_report process_cis_error_report; do
     sudo systemctl stop celery-worker@$queue.service
     sudo systemctl disable celery-worker@$queue.service
 done
